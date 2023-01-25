1da872ff2dbd9c934d84fae817011f48e12d0b9a
skywind3000 <skywind3000@163.com>
2023-01-25 11:52:06 +0800

    update LIBLR.py

--- LIBLR.py
+++ LIBLR.py
@@ -44,6 +44,10 @@ LOG_INFO = lambda *args: print('info:', *args)
 LOG_DEBUG = lambda *args: print('debug:', *args)
 LOG_VERBOSE = lambda *args: print('debug:', *args)
 
+# ignore log levels
+LOG_VERBOSE = lambda *args: 0
+LOG_DEBUG = lambda *args: 0
+
 
 #----------------------------------------------------------------------
 # GrammarError
