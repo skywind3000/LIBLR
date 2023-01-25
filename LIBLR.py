0bac10bf7ceb219897b7f1a8c9b9ddc1dc0d989a
skywind3000 <skywind3000@163.com>
2023-01-25 11:50:50 +0800

    update LIBLR.py

--- LIBLR.py
+++ LIBLR.py
@@ -13,6 +13,7 @@
 #   2023.01.23  skywind  grammar loader
 #   2023.01.24  skywind  LR(1) generator
 #   2023.01.25  skywind  LALR generator
+#   2023.01.25  skywind  Lexer & PDAInput
 #
 #======================================================================
 from __future__ import unicode_literals, print_function
@@ -3409,6 +3410,208 @@ class LALRAnalyzer (object):
         return 0
 
 
+#----------------------------------------------------------------------
+# LexerError
+#----------------------------------------------------------------------
+class LexerError (Exception):
+    def __init__ (self, error, line, column):
+        super(LexerError, self).__init__(error)
+        self.line = line
+        self.column = column
+
+
+#----------------------------------------------------------------------
+# lexer
+#----------------------------------------------------------------------
+class Lexer (object):
+
+    def __init__ (self):
+        self.rules = []
+        self.literal = {}
+        self.actions = {}
+        self.require = {}
+        self.intercept_literal = True
+
+    def clear (self):
+        self.rules.clear()
+        self.literal.clear()
+        self.require.clear()
+        return 0
+
+    def push_skip (self, pattern:str):
+        self.rules.append((None, pattern))
+        return 0
+
+    def _is_action (self, name:str) -> bool:
+        return (name.startswith('{') and name.endswith('}'))
+
+    def push_match (self, name:str, pattern:str):
+        name = name.strip()
+        if self._is_action(name):
+            action = name[1:-1].strip('\r\n\t ')
+            self.require[action] = len(self.rules)
+            self.rules.append((self.__handle_action, pattern, action))
+        else:
+            self.rules.append((name, pattern))
+        return 0
+
+    def push_import_match (self, name:str, key:str):
+        # print('import', name)
+        key = key.strip()
+        if name is None:
+            name = key
+        name = name.strip()
+        if key not in PATTERN:
+            raise LexerError('%s cannot be imported'%key, None, None)
+        return self.push_match(name, PATTERN[key])
+
+    def push_import_skip (self, key:str):
+        if key not in PATTERN:
+            raise LexerError('%s cannot be imported'%key, None, None)
+        return self.push_skip(PATTERN[key])
+
+    def push_literal (self, literal:str):
+        self.literal[literal] = cstring.string_quote(literal)
+
+    def __handle_action (self, text:str, action:str):
+        if text in self.literal:
+            if self.intercept_literal:
+                return (self.literal[text], text)
+        if action in self.actions:
+            fn = self.actions[action]
+            return fn(action, text)
+        if '*' in self.actions:
+            fn = self.actions['*']
+            return fn(action, text)
+        raise LexerError('missing action {%s}'%action, None, None)
+
+    def __handle_literal (self, text:str):
+        quoted = cstring.string_quote(text, True)
+        return (quoted, text)
+
+    def register (self, action:str, callback):
+        self.actions[action] = callback
+        return 0
+
+    def __convert_to_literal (self, token):
+        if cstring.string_is_quoted(token.name):
+            return token
+        name = token.value
+        if not isinstance(name, str):
+            return token
+        if name not in self.literal:
+            return token
+        t = Token(self.literal[name], name, token.line, token.column)
+        return t
+
+    def tokenize (self, code):
+        rules = [n for n in self.rules]
+        for literal in self.literal:
+            escape = re.escape(literal)
+            rules.append((self.__handle_literal, escape))
+        rules.append((self.__handle_mismatch, '.'))
+        last_line = 1
+        last_column = 1
+        try:
+            for token in ctoken_regex.tokenize(code, rules, '$'):
+                last_line = token.line
+                last_column = token.column
+                if isinstance(token.value, str):
+                    if not cstring.string_is_quoted(token.name):
+                        if token.value in self.literal:
+                            if self.intercept_literal:
+                                token = self.__convert_to_literal(token)
+                yield token
+        except LexerError as e:
+            e.line = last_line
+            e.column = last_column
+            raise e
+        return 0
+
+    def __handle_mismatch (self, text):
+        return (cstring.string_quote(text), text)
+
+
+#----------------------------------------------------------------------
+# push down automata input
+#----------------------------------------------------------------------
+class PushDownInput (object):
+
+    def __init__ (self, g:Grammar):
+        self.g: Grammar = g
+        self.lexer: Lexer = None
+        self.it = None
+        self.eof: bool = False
+        self.matcher = None
+
+    def open (self, code):
+        if isinstance(code, str):
+            self.lexer = self.__open_lexer()
+            self.it = self.lexer.tokenize(code)
+        elif hasattr(code, 'read'):
+            content = code.read()
+            self.lexer = self.__open_lexer()
+            self.it = self.lexer.tokenize(content)
+        elif hasattr(code, '__iter__'):
+            self.it = iter(code)
+        else:
+            raise TypeError('invalid source type')
+        self.eof = False
+        return 0
+
+    def read (self):
+        if self.eof:
+            return None
+        try:
+            token = next(self.it)
+        except StopIteration:
+            if not self.eof:
+                self.eof = True
+                return Token('$', '', None, None)
+            return None
+        if token.name == '$':
+            self.eof = True
+        return token
+
+    def __open_lexer (self):
+        lexer: Lexer = Lexer()
+        for scanner in self.g.scanner:
+            cmd: str = scanner[0].strip()
+            if cmd in ('ignore', 'skip'):
+                lexer.push_skip(scanner[1])
+            elif cmd == 'match':
+                lexer.push_match(scanner[1], scanner[2])
+            elif cmd == 'import':
+                lexer.push_import_match(scanner[1], scanner[2])
+        for terminal in self.g.terminal.values():
+            if cstring.string_is_quoted(terminal.name):
+                text = cstring.string_unquote(terminal.name)
+                lexer.push_literal(text)
+        lexer.register('*', self.__handle_lexer_action)
+        # lexer.push_literal('if')
+        return lexer
+
+    def __handle_lexer_action (self, action, text):
+        if self.matcher is not None:
+            clsname = self.matcher.__class__.__name__
+            if hasattr(self.matcher, action):
+                func = getattr(self.matcher, action)
+                return func(text)
+            else:
+                raise TypeError('method "%s" is undefined in %s'%(action, clsname))
+        return (cstring.string_quote('{%s}'%action), text)
+
+
+#----------------------------------------------------------------------
+# match action
+#----------------------------------------------------------------------
+class MatchAction (object):
+    def __init__ (self):
+        action = {}
+        for name, func in self.__class__.__dict__.items():
+            if not name.startswith('__'):
+                action[name] = func
+        self._MATCH_ACTION_ = action
 #----------------------------------------------------------------------
 # testing suit
 #----------------------------------------------------------------------
