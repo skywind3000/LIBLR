0dc3a8b7d043136954cfae8dfc98de5c3008c2bf
skywind3000 <skywind3000@163.com>
2023-01-27 11:12:55 +0800

    add json parser

---
+++ sample_json.py
@@ -0,0 +1,62 @@
+import sys
+import pprint
+import LIBLR
+
+from LIBLR import cstring
+
+class JsonAction:
+
+    def get1 (self, rule, args):
+        return args[1]
+
+    def get_string (self, rule, args):
+        return cstring.string_unquote(args[1])
+
+    def get_number (self, rule, args):
+        text = args[1]
+        if text.isdigit():
+            return int(text, 0)
+        return float(text)
+
+    def get_true (self, rule, args):
+        return True
+
+    def get_false (self, rule, args):
+        return False
+
+    def get_null (self, rule, args):
+        return None
+
+    def list_empty (self, rule, args):
+        return []
+
+    def list_one (self, rule, args):
+        return [args[1]]
+
+    def list_many (self, rule, args):
+        return args[1] + [args[3]]
+
+    def get_array (self, rule, args):
+        return args[2]
+
+    def item_pair (self, rule, args):
+        name = cstring.string_unquote(args[1])
+        value = args[3]
+        return (name, value)
+
+    def get_object (self, rule, args):
+        obj = {}
+        for k, v in args[2]:
+            obj[k] = v
+        return obj
+
+parser = LIBLR.create_parser_from_file('grammar/json.txt',
+                                       JsonAction(),
+                                       algorithm = 'lalr')
+
+text = open('sample/launch.json').read()
+
+pprint.pprint(parser(text))
+
+
+
